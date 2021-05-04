a403d9a374e1f3938f60a845e4df60f6f6597a62
yan0621 <panyan0621@gmail.com>
2021-05-04 21:56:11 +0800

    add new app st_assitant
    create dir

--- setup.py
+++ setup.py
@@ -10,3 +10,14 @@ setup(
         'flask',
     ],
 )
+
+setup(
+    name='st_assistant',
+    version='1.0.0',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'flask',
+    ],
+)
